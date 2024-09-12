8bec029b4de095166bdb2e614a67601f43662306
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-09-12 12:40:13 -0600

    more fixes

--- test_solve.cpp
+++ test_solve.cpp
@@ -25,6 +25,14 @@ int main() {
        -1.,  2., -1.,  2., -1.,  2., -1.,  2., -1.,  2., -1.,  2., -1.,
         2., -1.,  2., -1.,  2., -1.,  2., -1.,  2., -1.,  2., -1.,  2.};
 
+    float b[40] = {
+        1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18.,
+        19., 20., 21., 22., 23., 24., 25., 26., 27., 28., 29., 30., 31., 32., 33., 34.,
+        35., 36., 37., 38., 39., 40.
+    };
+
+    float x[40];
+
     /*
     Matrix looks like:
     A = [[-1,  1,  0, ...,  0,  0,  0],
@@ -55,6 +63,7 @@ int main() {
     */
 
     MKL_INT mtype = -2; // real symmetric indefinite
+    MKL_INT nrhs = 1;
 
     // Init handle
     _MKL_DSS_HANDLE_t handle[64];
@@ -124,17 +133,9 @@ int main() {
     }
     std::cout << "Factorization completed ... " << std::endl;
 
-    float b[40] = {
-        1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18.,
-        19., 20., 21., 22., 23., 24., 25., 26., 27., 28., 29., 30., 31., 32., 33., 34.,
-        35., 36., 37., 38., 39., 40.
-    };
-
-    float x[40];
-
     phase = 33;
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-             &n, a, ia, ja, perm, &nrhs, iparm, &msglvl, b, x, &error);
+    pardiso(handle, &maxfct, &mnum, &mtype, &phase,
+             &n, data, indptr, indices, perm, &nrhs, iparm, &msglvl, b, x, &error);
     if ( error != 0 ){
         std::cout << "ERROR during solution: " << error << std::endl;
         return 3;
